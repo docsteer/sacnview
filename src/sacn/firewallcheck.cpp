9b3889b8226defd3ceced12248fdda6105b39c61
Marcus Birkin <marcus.birkin@gmail.com>
2018-02-07 22:27:17 +0000

    Fix clang compiler warnings

--- src/sacn/firewallcheck.cpp
+++ src/sacn/firewallcheck.cpp
@@ -54,7 +54,8 @@ FwCheck::FwCheck_t FwCheck::isFWBlocked(QHostAddress ip)
 
     ret.allowed = (VARIANT_TRUE  == allowed.boolVal);
     ret.restricted = (VARIANT_TRUE  == restricted.boolVal);
-#endif //Q_WS_WIN
-
+#else
+    Q_UNUSED(ip);
+#endif
     return ret;
 }
