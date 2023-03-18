0b28ed80216897a42fadbaa0f5905e48ccd41cb2
Marcus Birkin <marcus.birkin@gmail.com>
2023-03-18 18:25:31 +0000

    Blake2 is C not C++, compile as such!

--- cmake/blake2.cmake
+++ cmake/blake2.cmake
@@ -16,14 +16,14 @@ FetchContent_MakeAvailable(blake2)
 
 # Check if SSE2 is supported by target
 include(CheckCCompilerFlag)
-if (CMAKE_CXX_COMPILER_ID MATCHES "MSVC")
-    if(MSVC_CXX_ARCHITECTURE_ID MATCHES 64)
+if (CMAKE_C_COMPILER_ID MATCHES "MSVC")
+    if(MSVC_C_ARCHITECTURE_ID MATCHES 64)
         set(SSE2_SUPPORTED 1)
     elseif()
-        check_cxx_compiler_flag("/arch:SSE2" SSE2_SUPPORTED)
+        check_c_compiler_flag("/arch:SSE2" SSE2_SUPPORTED)
     endif()
 else()
-    check_cxx_compiler_flag("-msse2" SSE2_SUPPORTED)
+    check_c_compiler_flag("-msse2" SSE2_SUPPORTED)
 endif()
 if(SSE2_SUPPORTED)
     set(BLAKE2_INCLUDE_DIR ${blake2_SOURCE_DIR}/sse)
@@ -33,7 +33,7 @@ endif()
 
 file(GLOB_RECURSE BLAKE2_HEADERS CONFIGURE_DEPENDS "${BLAKE2_INCLUDE_DIR}/*.h")
 file(GLOB_RECURSE BLAKE2_SOURCES CONFIGURE_DEPENDS "${BLAKE2_INCLUDE_DIR}/*.c")
-set_source_files_properties(${BLAKE2_SOURCES} PROPERTIES LANGUAGE CXX)
+set_source_files_properties(${BLAKE2_SOURCES} PROPERTIES LANGUAGE C)
 add_library(Blake2 STATIC
     ${BLAKE2_HEADERS}
     ${BLAKE2_SOURCES}
