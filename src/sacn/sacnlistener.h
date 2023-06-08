bc69a960ebdc4fa2de8d3981fa0c6f43d6980f70
Richard Thompson <richard.thompson@etcconnect.com>
2023-06-08 14:05:53 +0100

    Apply clangformat

--- src/sacn/sacnlistener.h
+++ src/sacn/sacnlistener.h
@@ -31,17 +31,17 @@ Q_DECLARE_METATYPE(QHostAddress)
 
 struct sACNMergedAddress
 {
-    sACNMergedAddress() {
-        level = -1;
-        winningSource = NULL;
-        changedSinceLastMerge = false;
-        winningPriority = 0;
-    }
-    int level;
-    sACNSource *winningSource;
-    QSet<sACNSource *> otherSources;
-    bool changedSinceLastMerge;
-    int winningPriority;
+  sACNMergedAddress() {
+    level = -1;
+    winningSource = NULL;
+    changedSinceLastMerge = false;
+    winningPriority = 0;
+  }
+  int level;
+  sACNSource* winningSource;
+  QSet<sACNSource*> otherSources;
+  bool changedSinceLastMerge;
+  int winningPriority;
 };
 
 typedef QVector<sACNMergedAddress> sACNMergedSourceList;
@@ -53,106 +53,106 @@ typedef QVector<sACNMergedAddress> sACNMergedSourceList;
  */
 class sACNListener : public QObject
 {
-    Q_OBJECT
+  Q_OBJECT
 public:
-    sACNListener(int universe, QObject *parent = 0);
-    virtual ~sACNListener();
+  sACNListener(int universe, QObject* parent = 0);
+  virtual ~sACNListener();
 
-    /**
-     * @brief universe
-     * @return the universe which this listener is listening for
-     */
-    int universe() const { return m_universe; }
-    /**
-     * @brief mergedLevels
-     * @return an sACNMergerdSourceList, a list of merged address structures, allowing you to see
-     * the result of the merge algorithm together with all the sub-sources, by address
-     */
-    sACNMergedSourceList mergedLevels() {
-        QMutexLocker mergeLocker(&m_merged_levelsMutex);
-        return m_merged_levels;
-    }
+  /**
+   * @brief universe
+   * @return the universe which this listener is listening for
+   */
+  int universe() const { return m_universe; }
+  /**
+   * @brief mergedLevels
+   * @return an sACNMergerdSourceList, a list of merged address structures, allowing you to see
+   * the result of the merge algorithm together with all the sub-sources, by address
+   */
+  sACNMergedSourceList mergedLevels() {
+    QMutexLocker mergeLocker(&m_merged_levelsMutex);
+    return m_merged_levels;
+  }
 
-    int sourceCount() const { return static_cast<int>(m_sources.size()); }
-    sACNSource *source(int index) { return m_sources[index];}
-    const std::vector<sACNSource*> getSourceList() const { return m_sources; }
+  int sourceCount() const { return static_cast<int>(m_sources.size()); }
+  sACNSource* source(int index) { return m_sources[index]; }
+  const std::vector<sACNSource*> getSourceList() const { return m_sources; }
 
-    /**
-     *  @brief processDatagram Process a suspected sACN datagram.
-     * This allows other listeners to pass on unicast datagrams for other universes
-     *
-     */
-    Q_INVOKABLE void processDatagram(const QByteArray &data, const QHostAddress &destination, const QHostAddress &sender);
+  /**
+   *  @brief processDatagram Process a suspected sACN datagram.
+   * This allows other listeners to pass on unicast datagrams for other universes
+   *
+   */
+  Q_INVOKABLE void processDatagram(const QByteArray& data, const QHostAddress& destination, const QHostAddress& sender);
 
-    // Diagnostic - the number of merge operations per second
+  // Diagnostic - the number of merge operations per second
 
-    int mergesPerSecond() const { return (m_mergesPerSecond > 0) ? m_mergesPerSecond : 0;}
+  int mergesPerSecond() const { return (m_mergesPerSecond > 0) ? m_mergesPerSecond : 0; }
 
-    /**
-     *  @brief getBindStatus Get interface bind status of listener
-     *  @return A struct of bind types and status
-     */
-    sACNRxSocket::sBindStatus getBindStatus() const { return m_bindStatus; }
+  /**
+   *  @brief getBindStatus Get interface bind status of listener
+   *  @return A struct of bind types and status
+   */
+  sACNRxSocket::sBindStatus getBindStatus() const { return m_bindStatus; }
 
-    /**
-     * @brief Force the listener to performa a full merge
-     */
-    void doFullMerge() { m_mergeAll = true; }
+  /**
+   * @brief Force the listener to performa a full merge
+   */
+  void doFullMerge() { m_mergeAll = true; }
 
 public slots:
-    void startReception();
-    void monitorAddress(int address, const QObject *owner) {
-        QMutexLocker locker(&m_monitoredChannelsMutex);
-        m_monitoredChannels.insert(owner, address);
-        connect(owner, &QObject::destroyed, this, [this](const QObject *owner) {
-            QMutexLocker locker(&m_monitoredChannelsMutex);
-            m_monitoredChannels.remove(owner);
-        });
-    }
-    void unMonitorAddress(int address, const QObject *owner) {
-        QMutexLocker locker(&m_monitoredChannelsMutex);
-        m_monitoredChannels.remove(owner, address);
-    }
+  void startReception();
+  void monitorAddress(int address, const QObject* owner) {
+    QMutexLocker locker(&m_monitoredChannelsMutex);
+    m_monitoredChannels.insert(owner, address);
+    connect(owner, &QObject::destroyed, this, [this](const QObject* owner) {
+      QMutexLocker locker(&m_monitoredChannelsMutex);
+      m_monitoredChannels.remove(owner);
+      });
+  }
+  void unMonitorAddress(int address, const QObject* owner) {
+    QMutexLocker locker(&m_monitoredChannelsMutex);
+    m_monitoredChannels.remove(owner, address);
+  }
 signals:
-    void listenerStarted(int universe);
-    void sourceFound(sACNSource *source);
-    void sourceLost(sACNSource *source);
-    void sourceResumed(sACNSource *source);
-    void sourceChanged(sACNSource *source);
-    void levelsChanged();
-    void dataReady(int address, QPointF data);  // Used by ScopeWidget
+  void listenerStarted(int universe);
+  void sourceFound(sACNSource* source);
+  void sourceLost(sACNSource* source);
+  void sourceResumed(sACNSource* source);
+  void sourceChanged(sACNSource* source);
+  void levelsChanged();
+  void dataReady(int address, QPointF data);  // Used by ScopeWidget
 
 private slots:
-    void readPendingDatagrams();
-    void performMerge();
-    void checkSourceExpiration();
-    void sampleExpiration();
+  void readPendingDatagrams();
+  void performMerge();
+  void checkSourceExpiration();
+  void sampleExpiration();
 
 private:
-    QMutex m_processMutex;
-    void startInterface(const QNetworkInterface &iface);
-    std::list<sACNRxSocket *> m_sockets;
-    std::vector<sACNSource *> m_sources;
-    int m_last_levels[MAX_DMX_ADDRESS];
-    sACNMergedSourceList m_merged_levels;
-    QMutex m_merged_levelsMutex;
-    int m_universe;
-    // The per-source hold last look time
-    int m_ssHLL = 1000;
-    // Are we in the initial sampling state
-    bool m_isSampling = true;
-    QTimer *m_initalSampleTimer = nullptr;
-    QTimer *m_mergeTimer = nullptr;
-    QElapsedTimer m_elapsedTime;
-    int m_predictableTimerValue;
-    QMutex m_monitoredChannelsMutex;
-    QMultiMap<const QObject*, int> m_monitoredChannels;
-    bool m_mergeAll; // A flag to initiate a complete remerge of everything
-    unsigned int m_mergesPerSecond = 0;
-    int m_mergeCounter;
-    QElapsedTimer m_mergesPerSecondTimer;
+  QMutex m_processMutex;
+  void startInterface(const QNetworkInterface& iface);
+  std::list<sACNRxSocket*> m_sockets;
+  std::vector<sACNSource*> m_sources;
+  int m_last_levels[MAX_DMX_ADDRESS];
+  sACNMergedSourceList m_merged_levels;
+  QMutex m_merged_levelsMutex;
+  int m_universe;
+  // The per-source hold last look time
+  int m_ssHLL = 1000;
+  // Are we in the initial sampling state
+  bool m_isSampling = true;
+  QTimer* m_initalSampleTimer = nullptr;
+  QTimer* m_mergeTimer = nullptr;
+  QElapsedTimer m_elapsedTime;
+  int m_predictableTimerValue;
+  QMutex m_monitoredChannelsMutex;
+  QMultiMap<const QObject*, int> m_monitoredChannels;
+  bool m_mergeAll; // A flag to initiate a complete remerge of everything
+  unsigned int m_mergesPerSecond = 0;
+  int m_mergeCounter;
+  QElapsedTimer m_mergesPerSecondTimer;
 
-    sACNRxSocket::sBindStatus m_bindStatus;
+  sACNRxSocket::sBindStatus m_bindStatus;
 };
 
 
