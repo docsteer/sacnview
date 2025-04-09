7a5d423b46ad8db69431acd9c32fc5ecd0e97179
Tom Steer <me@tomsteer.net>
2025-04-09 20:23:25 +0100

    Correct case of DarkStyle

--- themes/sources.cmake
+++ themes/sources.cmake
@@ -2,7 +2,7 @@
 list(APPEND SACNVIEW_SOURCES
     ${CMAKE_CURRENT_LIST_DIR}/themes.cpp
     ${CMAKE_CURRENT_LIST_DIR}/darktheme.cpp
-    ${CMAKE_CURRENT_LIST_DIR}/Qt-Frameless-Window-DarkStyle/darkstyle.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/Qt-Frameless-Window-DarkStyle/DarkStyle.cpp
 )
 
 list(APPEND SACNVIEW_RCC
