a99947f6daa6149c844f1b894e205e194412e12e
Tom Steer <toms@pharoscontrols.com>
2025-05-14 22:02:59 +0100

    Check SimplySign tool before signing
    The SimplySign tool is needed to run windows signing;
    check if it's absent to avoid CI build errors.

--- install/win64/sign_installer.bat
+++ install/win64/sign_installer.bat
@@ -1,3 +1,7 @@
 :: Invoke signtool to sign the generated binary
+:: Only if the SimplySign desktop tool is running (not in CI)
+tasklist /fi "imagename eq SimplySignDesktop.exe" | findstr /B /I /C:"SimplySignDesktop.exe " >NUL
+
+IF ERRORLEVEL 1 exit 0
 
 signtool.exe sign /n "Open Source Developer, Thomas Steer" /fd SHA256 /tr http://timestamp.digicert.com /td SHA256 %1
