431537593abc709b3d8c8456905c35beb2e48213
Tom Steer <me@tomsteer.net>
2026-09-19 09:10:55 +0100

    Complete DMG signing work

--- install/macos/create_sacnview_dmg.sh
+++ install/macos/create_sacnview_dmg.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 # Create a DMG for sACNView on macOS
+#
+# Expects an environment variable KEY_FILE to be set to the path of a .p8 file for signing the DMG.
+
+codesign --force --deep --verbose --timestamp --entitlements install/macos/Entitlements.plist --options runtime -s "Developer ID Application: Thomas Steer (8JU6XWK784)" out/build/macos/Release/sACNView.app
+
+RESULT=$?
+if [ $RESULT -ne 0 ]; then
+    echo "Error Codesigning sACNView.app"
+    exit $RESULT
+fi
 
 create-dmg \
   --volname "sACNView" \
@@ -11,6 +21,25 @@ create-dmg \
   --icon "sACNView.app" 200 190 \
   --hide-extension "sACNView.app" \
   --app-drop-link 600 185 \
-  --codesign "Apple Distribution: Carallon ltd (7J5M6EPN5V)" \
+  --codesign "Apple Distribution: Thomas Steer (8JU6XWK784)" \
+  --overwrite \
   "sACNView.dmg" \
   "out/build/macos/Release/sACNView.app"
+
+RESULT=$?
+if [ $RESULT -ne 0 ]; then
+    echo "Error creating sACNView.dmg"
+    exit $RESULT
+fi
+
+xcrun notarytool store-credentials "sign-sacnview" --key $KEY_FILE --key-id 9884V35HM7 --issuer 69a6de7c-7b58-47e3-e053-5b8c7c11a4d1
+
+RESULT=$?
+if [ $RESULT -ne 0 ]; then
+    echo "Error storing credentials for notarytool"
+    exit $RESULT
+fi
+
+xcrun notarytool submit "sACNView.dmg" --keychain-profile "sign-sacnview" --wait
+
+xcrun stapler staple "sACNView.dmg"
